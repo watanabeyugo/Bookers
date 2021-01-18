a1f6c0c8c81cdcd4cb242e761d51273685a60e38
EC2 Default User <ec2-user@ip-172-31-92-143.ec2.internal>
2021-01-18 04:53:20 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+    root 'books#top'
+    get 'books' =>'books#index'
+    post 'books' => 'books#create'
+    get 'books/:id' => 'books#show', as: 'book'
+    get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+    patch 'books/:id' => 'books#update', as: 'update_book'
+    put 'books/:id' => 'books#update'
+    delete 'books/:id' => 'books#destroy'
+end
